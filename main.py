a35d9a9a5192a1043a5cfcac1bfb3d3b1d9e36b7
Michael Smith <michael.smith.0@outlook.com>
2023-12-18 16:14:03 +1000

    .
    .

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ import requests, csv #py -m pip install requests
 client_id = ""
 client_secret = ""
 
+def get_access_token(url, client_id, client_secret):
+    response = requests.post(
+        url,
+        data={"grant_type": "client_credentials", "scope": "fw-fl2-api-au"},
+        auth=(client_id, client_secret),
+    )
+    return response.json()["access_token"]
+    
+# assign to a variable to pass to delete     
+print(get_access_token("https://identity.fastway.org/connect/token", client_id, client_secret))
+
+
+
+## Doesnt work yet
 def iterate_first_column(csv_file_path):
     with open(csv_file_path, 'r') as file:
         # Create a CSV reader object
@@ -15,20 +29,11 @@ def iterate_first_column(csv_file_path):
                 first_column_value = row[0]
                 print(first_column_value) #add delete action here
                 
-# assign to a variable to pass to delete                
-print(get_access_token("https://identity.fastway.org/connect/token", client_id, client_secret))
-
-
-
-
-
-# path to conid csv ****concept*****
+           
+# Provide the path to your CSV file
 csv_file_path = 'labels.csv'
 
-def get_access_token(url, client_id, client_secret):
-    response = requests.post(
-        url,
-        data={"grant_type": "client_credentials", "scope": "fw-fl2-api-au"},
-        auth=(client_id, client_secret),
-    )
-    return response.json()["access_token"]
+# Call the function with the CSV file path
+# iterate_first_column(csv_file_path)
+
+
