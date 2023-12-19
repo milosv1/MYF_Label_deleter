fc33566346169a07143d8e5a97d37ba369385d9f
Michael Smith <michael.smith.0@outlook.com>
2023-12-19 10:23:39 +1000

    Working exmaple

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import requests, csv #py -m pip install requests
+import requests, csv, os #py -m pip install requests
+
+#location of main.py
+folder_path = os.path.dirname(os.path.abspath(__file__))
 
 #creds
-client_id = ""
 client_secret = ""
+client_id = ""
 
 def get_access_token(url, client_id, client_secret):
     response = requests.post(
@@ -10,30 +13,32 @@ def get_access_token(url, client_id, client_secret):
         data={"grant_type": "client_credentials", "scope": "fw-fl2-api-au"},
         auth=(client_id, client_secret),
     )
-    return response.json()["access_token"]
+    if response.json()["access_token"]:
+        return response.json()["access_token"]
+    else:
+        raise Exception(f"Failed to get access token.")
     
-# assign to a variable to pass to delete     
-print(get_access_token("https://identity.fastway.org/connect/token", client_id, client_secret))
+def make_api_request(api_url, access_token):
+    # Make API request using the OAuth token
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'Content-Type': 'application/json',
+    }
+    response = requests.delete(api_url, headers=headers)
+    if response.status_code == 200:
+        return response
+    else:
+        return(f"API request failed with status code {response.status_code}. Response: {response.text}")
 
-
-
-## Doesnt work yet
-def iterate_first_column(csv_file_path):
-    with open(csv_file_path, 'r') as file:
-        # Create a CSV reader object
-        csv_reader = csv.reader(file)
-        
-        # Iterate over each row and print the first column
-        for row in csv_reader:
-            if row:  # Check if the row is not empty
-                first_column_value = row[0]
-                print(first_column_value) #add delete action here
+def iterate_first_column(access_token):
+    with open(folder_path + '\labels.csv') as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        for row in readCSV:
+            print(row[0])
+            print(make_api_request(f"https://api.myfastway.com.au/api/consignments/{row[0]}/reason/3", access_token))
                 
+
+access_token = get_access_token("https://identity.fastway.org/connect/token", client_id, client_secret)
            
-# Provide the path to your CSV file
-csv_file_path = 'labels.csv'
-
-# Call the function with the CSV file path
-# iterate_first_column(csv_file_path)
-
-
+# Call the delete function iterating over the list in the csv
+iterate_first_column(access_token)
