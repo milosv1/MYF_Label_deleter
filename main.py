255a88dddc530025065ff52bc380ad5bf6207ff9
Milos <milosvuksanovic53@gmail.com>
2023-12-19 20:30:45 +1100

    Made adjustments to iterate_first_column

--- main.py
+++ main.py
@@ -29,21 +29,29 @@ def make_api_request(api_url, access_token):
         'Content-Type': 'application/json',
     }
     response = requests.delete(api_url, headers=headers)
-    if response.status_code == 200:
-        return response
-    else:
-        return(f"API request failed with status code {response.status_code}. Response: {response.text}")
-
+    # if response.status_code == 200:
+    #     return response
+    # else:
+    #     return(f"API request failed with status code {response.status_code}. Response: {response.text}")
+    return response
 
 def iterate_first_column(access_token):
     conids_list = []
     with open(folder_path + '\conids.csv') as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
         for row in readCSV:
-            print(row[0])
-            conids_list.append(row[0])
-            print(make_api_request(f"https://uat.api.myfastway.com.au/api/consignments/{row[0]}/reason/3", access_token))
-            print(f"Successfully deleted {row[0]}!")
+            # if row is not empty...
+            #TODO - try and get content of each cell, then save them onto the list and into other csv file.
+            if row:       
+                conid = row[0]
+                conids_list.append(conid)
+                response = make_api_request(f"https://uat.api.myfastway.com.au/api/consignments/{conid}/reason/3", access_token)
+                if response.status_code == 200:
+                    print(f"Successfully deleted {conid}")
+                else:
+                    print(f"Delete request has failed with {response.status_code}. Response: {response.text}")         
+            else:
+                print("Cannot be empty!")
 
     return conids_list        
 
