924c1943f39a231bfaefc5704b4afe43aabba569
Michael Smith <michael.smith.0@outlook.com>
2023-12-21 12:30:49 +1000

    4/5 Updates
    1.	Fixed duplicate myfastway API calls in log writing function.
    2.	Made log writer more elegant.
    3.	Added dynamic log names based on Unix time.
    4.	Added Global  variable for URL locations.
    5.	Fix some other bugs

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import requests, csv, os #py -m pip install requests
+import requests, csv, os, time #py -m pip install requests
 
-
+#GLOBALS
 #location of main.py
 folder_path = os.path.dirname(os.path.abspath(__file__))
-
-
-#Enter your Myfastway Credentials (UAT)
+log__file_name = str(time.time()) + '_logs.txt' #log file naming
 client_id = ""
 client_secret = ""
+myfastway_url = "uat.api.myfastway.com.au/"
+identity_url = "https://uat.identity.fastway.org/connect/token"
 
 def get_access_token(url, client_id, client_secret):
     response = requests.post(
@@ -19,7 +19,6 @@ def get_access_token(url, client_id, client_secret):
         return response.json()["access_token"]
     else:
         raise Exception(f"Failed to get access token.")
-
 
 def make_api_request(api_url, access_token):
     # Make API request using the OAuth token
@@ -33,6 +32,11 @@ def make_api_request(api_url, access_token):
     # else:
     #     return(f"API request failed with status code {response.status_code}. Response: {response.text}")
     return response
+    
+#Write lines to a dynamic log file
+def write_logs(action_reponse):
+    with open(folder_path +  '\\' + log__file_name, 'a') as txt:
+        txt.write(action_reponse)
 
 def iterate_first_column(access_token):
     conids_list = []
@@ -43,17 +47,18 @@ def iterate_first_column(access_token):
             #TODO - try and get content of each cell, then save them onto the list and into other csv file.
             if row:       
                 conid = row[0]
-                conids_list.append(conid)
-                response = make_api_request(f"https://uat.api.myfastway.com.au/api/consignments/{conid}/reason/3", access_token)
+                response = make_api_request(f"https://{myfastway_url}api/consignments/{conid}/reason/3", access_token)
                 if response.status_code == 200:
                     print(f"Successfully deleted {conid}")
+                    write_logs(f"Successfully deleted: {conid} with {response.status_code}\n")
+                    conids_list.append(conid)
                 else:
-                    print(f"Delete request has failed with {response.status_code}. Response: {response.text}")         
+                    print(f"Delete request has failed with {response.status_code}. Response: {response.text}")  
+                    write_logs(f"Failed to delete: {conid} with {response.status_code} {response.json()}\n")
             else:
                 print("Cannot be empty!")
 
     return conids_list        
-
 
 #This function adds any deleted conids to a new csv file which acts as a small database
 def add_deleted_conids_to_csv(conids_list):
@@ -63,39 +68,12 @@ def add_deleted_conids_to_csv(conids_list):
             writeCSV.writerow([conid])
 
 
-def write_logs(conid, access_token):
-    with open(folder_path + '\logs.txt', 'a') as txt:
-        response = make_api_request(f"https://uat.api.myfastway.com.au/api/consignments/{conid}/reason/3", access_token)
-       
-        print(f"Response for {conid}: {response.status_code} - {response.text}")
-       
-        try:
-            response_json = response.json()
-        except ValueError:
-            response_json = None
-
-        if response.status_code == 200:
-            txt.write(f"Successfully deleted: {conid} with {response.status_code}\n")
-            if response_json:
-                txt.write(f"Response: {response_json}\n")
-        else:
-            txt.write(f"Failed to delete: {conid} with {response.status_code}\n")
-            if response_json:
-                txt.write(f"Error Response: {response_json}\n")
-
-        txt.write('\n')
-
-
-access_token = get_access_token("https://uat.identity.fastway.org/connect/token", client_id, client_secret)
+access_token = get_access_token(identity_url, client_id, client_secret)
            
 # Call the delete function iterating over the list in the csv
-#iterate_first_column(access_token)
-
 conids_list = iterate_first_column(access_token)
 
 #Call function to write deleted conids to new csv file
 add_deleted_conids_to_csv(conids_list)
 
-#For each conid in the list, write a log to the logs.txt file
-for conid in conids_list:
-    write_logs(conid, access_token)
+
